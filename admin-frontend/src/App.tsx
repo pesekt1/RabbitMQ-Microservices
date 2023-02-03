f38dc0c39bb4c092ef5c804a3309a4b6f1ce44f0
Tomas Pesek <pesekt@gmail.com>
2023-02-03 10:37:13 +0100

    removed dependency

--- admin-frontend/src/App.tsx
+++ admin-frontend/src/App.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./App.css";
 import Products from "./admin/Products";
 import { BrowserRouter, Route } from "react-router-dom";
-import Main from "./main/Main";
 import ProductsCreate from "./admin/ProductsCreate";
 import ProductsEdit from "./admin/ProductsEdit";
 
